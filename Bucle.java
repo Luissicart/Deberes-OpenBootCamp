8b965f82025e4806e3f46fe488b6e16fc279977c
Luissicart <109523221+Luissicart@users.noreply.github.com>
2022-08-01 20:33:18 +0200

    Add files via upload

---
+++ Bucle.java
@@ -0,0 +1,18 @@
+package com.openboot;
+
+
+public class Bucle {
+    public static void main(String[] args) {
+
+        String[] nombre = new String[] {"Luis", "Abuela", "Arlenis", "Jaime"};
+
+        for (int i = 0; i < nombre.length; i++) {
+            System.out.println(nombre[i] + " " + nombre[i + 1] + " " + nombre[i + 2] + " " + nombre[i + 3]);
+        }
+        }
+    }
+
+
+
+
+
